d35bc7bf7d32495638b369c774f4f74ca1f031ca
rkurbanov <ray.kurbanov@gmail.com>
2023-01-24 15:48:25 -0600

    updated server side

--- server/server.js
+++ server/server.js
@@ -3,13 +3,22 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, "..", "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile("index.html", {
-    root: path.join(__dirname, "..", "build"),
-  });
-  // res.sendFile(path.join(__dirname, "../build", "index.html"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.static(path.join(__dirname, "..", "build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile("index.html", {
+//     root: path.join(__dirname, "..", "build"),
+//   });
+//   // res.sendFile(path.join(__dirname, "../build", "index.html"));
+// });
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
 app.post("/", (req, res) => {
