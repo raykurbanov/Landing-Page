38892940c00ade3f2b52af9b9195fbae4c800a3f
rkurbanov <ray.kurbanov@gmail.com>
2023-01-24 15:47:49 -0600

    udpated route

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, "..", "build")));
 
-app.get("/homepage", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile("index.html", {
     root: path.join(__dirname, "..", "build"),
   });
