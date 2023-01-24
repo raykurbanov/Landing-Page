517b849d16bc1471a566ed57bdcc6b9f57513833
rkurbanov <ray.kurbanov@gmail.com>
2023-01-24 15:49:37 -0600

    updated server side

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
 app.post("/", (req, res) => {
