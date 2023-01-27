d1e23ffd9e65e7483073587c34442a30ac6336a2
Ray Kurbanov <ray.kurbanov@gmail.com>
2023-01-27 12:56:37 -0600

    added auth0

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import LandingPage from "./Pages/LandingPage/LandingPage";
 import { LandingPageProvider } from "./context/LandingPageContext";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const App = () => {
   return (
-    <LandingPageProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/about" element={<LandingPage />} />
-        </Routes>
-      </Router>
-    </LandingPageProvider>
+    <Auth0Provider>
+      <LandingPageProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/about" element={<LandingPage />} />
+          </Routes>
+        </Router>
+      </LandingPageProvider>
+    </Auth0Provider>
   );
 };
 
